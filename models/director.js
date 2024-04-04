d69f4da1ce198869f7b0756ab898a70cb499248a
Adrián García Arranz <114479608+aadrigaar@users.noreply.github.com>
2024-04-04 15:17:39 +0200

    Update director.js

--- models/director.js
+++ models/director.js
@@ -1,1 +1,12 @@
+const mongoose = require('mongoose');
+
+const directorSchema = new mongoose.Schema({
+  id_director: { type: Number },
+  nombre: { type: String },
+  fecha_nacimiento: { type: String }
+});
+
+const Director = mongoose.model('Director', directorSchema);
+
+module.exports = Director;
 
